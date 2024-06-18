dec4109d1bc09d3b7711413b96073eea1f879089
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-18 11:20:12 -0400

    soft appearance of text inside Picture

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import './PictureStyle.css';
+import React, { useRef } from 'react';
+//import descriptions
 import { introductionData } from '../../models/data';
+//import intersection observer hook
+import useIntersectionObsever from '../../utils/useIntersectionObserver';
+//import main image
 import picture from '../../assets/images/picture.png'; 
+//import styles
+import './PictureStyle.css';
 
 const Picture = () => {
+
+  const pictureRef = useRef(null);
+  const isInView = useIntersectionObsever(pictureRef);
+
   return (
     <section>
 
     <div className="card-section gradient">
             {/* picture container */}
-        <div className='image-container'>
+        <div className='image-container' ref={pictureRef}>
           <img src={picture} alt="Nika's Picture" />
         </div>
           {/* text container */}
-        <div className="text-container">
+        <div className={`text-container ${isInView ? 'appear' : ''}`}>
           {/* greeting */}
           <div className="greeting">{introductionData.greeting}</div>
           {/* introduction */}
