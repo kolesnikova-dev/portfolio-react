b5638d21b6f71a079262ee644a369ebd8107d0c9
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-18 14:45:07 -0400

    added comments to Certifications

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import text and symbols
 import { certificationsData, symbols } from '../../models/data';
+//import components
 import Card from '../Card/Card';
 import Button from '../Button/Button';
+//import style
 import './CertificationsStyle.css';
-import { useState } from 'react';
+
 
 const Certifications = () => {
 
+  // initialize state for setting an overlay with a certification
   const [overlay, setOverlay] = useState(null);
 
+  // display overlay
   const showOverlay = (credential) => {
+    // set current credential as overlay
     setOverlay(credential);
-    console.log(credential);
+    // console.log(credential);
   };
 
+  // stop displaaying an overlay
   const closeOverlay = () => {
+    // set state to null
     setOverlay(null);
   };
 
@@ -28,29 +36,37 @@ const Certifications = () => {
       
       <div className='card-section certifications-section'>
 
+        {/* loop through all credentials and return a card per credential */}
         {Object.values(certificationsData).map((item, index) => (
           <Card 
             thisClass={'cert'} 
             data={item} 
-            key={index} 
+            key={index}
+            // if clicked show relevant credential 
             action={()=>showOverlay(item)}
           />
         ))}
 
       </div>
 
+      {/* if overlay exists return overlay with a credential and a closing button */}
       {overlay && (
         <div className='overlay'>
           <Button 
             thisClass={'close-overlay-button'} 
+            // close buttom icon
             symbol={symbols.closeButton} 
+            //onclick close current overlay
             action={closeOverlay} 
           />
           
+          {/* overlay container */}
           <div className='overlay-image-container'>
+            {/* current credential with its name as alternative */}
             <img src={overlay.image} alt={overlay.name} />
           </div>
           
+          {/* short description */}
           <div className='overlay-details-container'>
             <div className="overlay-details">
             {overlay.details}
