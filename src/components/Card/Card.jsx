94c1b9b9c94876bd98a28eda408a72800e8f1396
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-03 16:49:48 -0400

    add useIntersectionObserver hook to Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Card.css';
 import './CardNestHubStyle.css'
+import ImageContainer from '../ImageContainer/ImageContainer';
+import useIntersectionObserver from '../../utils/useIntersectionObserver';
 
 const Card = ({data, thisClass, action}) => {
+
+  const cardRef = useRef();
+  const isInView = useIntersectionObserver(cardRef);
+
+
+  const perScholasLogo = {
+    src: "https://upload.wikimedia.org/wikipedia/commons/e/e9/Per-Scholas-Secondary-Logo-Dark.png",
+    alt: "Per Scholas logo"
+  }
   // console.log('data inside card', data);
 
   const imagesJXS = !data.details && 
@@ -11,15 +22,18 @@ const Card = ({data, thisClass, action}) => {
   </div>;
 
   return (
-    <div className={`card ${thisClass}`} onClick={action ? ()=>action(data) : null}>
-      {
-      data.name.includes('Software Engineering Training') && 
-      <div className='per-scholas-logo'>
-        <img 
-          src="https://upload.wikimedia.org/wikipedia/commons/e/e9/Per-Scholas-Secondary-Logo-Dark.png" 
-          alt="Per Scholas logo"
-        /></div>
-      }
+    <div 
+      ref={cardRef}
+      className={`card ${thisClass} ${isInView ? 'delayedAppear' : ''}`} 
+      onClick={action ? ()=>action(data) : null}>
+
+      { data.name.includes('Software Engineering Training') && 
+        <ImageContainer 
+          thisClass={'per-scholas-logo'} 
+          src={perScholasLogo.src} 
+          alt={perScholasLogo.alt} 
+        /> }
+
       <div className='card-first-child'>{data.name}</div>
       <div className='card-second-child'>{data.level}</div>
       
