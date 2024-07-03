633c92695b362ccd8668af76ae2f457488a544b5
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-03 14:04:37 -0400

    remove typescript checker

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-//import  symbols
-import { symbols } from '../../models/data';
 //import components
+import Overlay from '../Overlay/Overlay';
 import Card from '../Card/Card';
-import Button from '../Button/Button';
 //import styles
 import './CertificationsStyle.css';
 import './CertificationsNestHubStyle.css'
@@ -51,33 +49,8 @@ const Certifications = ({ data, header }) => {
       </div>
 
       {/* if overlay exists return overlay with a credential and a closing button */}
-      {overlay && (
-        <div className='overlay'>
-          <Button 
-            thisClass={'close-overlay-button'} 
-            // close buttom icon
-            symbol={symbols.closeButton} 
-            //onclick close current overlay
-            action={closeOverlay} 
-          />
-          
-          {/* overlay container */}
-          <div className='overlay-image-container'>
-            {/* current credential with its name as alternative */}
-            <img src={overlay.image} alt={overlay.name} />
-          </div>
-          
-          {/* short description */}
-          <div className='overlay-details-container'>
-            <div className="overlay-details">
-            {overlay.details}
-            </div>
-          </div>
+      { overlay && <Overlay overlay={overlay}/> }
 
-
-
-        </div>
-    )}
     </section>
   );
 };
---
+++ src/components/Overlay/Overlay.jsx
@@ -0,0 +1,38 @@
+//import components
+import Button from '../Button/Button';
+//import  symbols
+import { symbols } from '../../models/data';
+//import styles
+import './OverlayStyle.css';
+
+const Overlay = ({ overlay }) => {
+  return (
+    <div className='overlay'>
+    <Button 
+      thisClass={'close-overlay-button'} 
+      // close buttom icon
+      symbol={symbols.closeButton} 
+      //onclick close current overlay
+      action={closeOverlay} 
+    />
+    
+    {/* overlay container */}
+    <div className='overlay-image-container'>
+      {/* current credential with its name as alternative */}
+      <img src={overlay.image} alt={overlay.name} />
+    </div>
+    
+    {/* short description */}
+    <div className='overlay-details-container'>
+      <div className="overlay-details">
+      {overlay.details}
+      </div>
+    </div>
+
+
+
+  </div>
+  )
+}
+
+export default Overlay;
