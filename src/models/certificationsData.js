11c86462e2857e567425e160457b4ac57b2f8e9c
Nika Kolesnikova <firstnamenika@gmail.com>
2024-08-08 12:05:36 -0400

    update certification.details to be an array of strings that will be later displayed with FormattedArray component

--- src/models/certificationsData.js
+++ src/models/certificationsData.js
@@ -1,80 +1,95 @@
 export const certificationsData = {
+    ts2024: {
+        name: 'TypeScript Course',
+        level: '2024 - Codecademy',
+        image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/ts2024.png?raw=true',
+        details:  [
+            `Learnt how to define and implement functions with typed parameters and return types, ensuring that 
+             functions behave as expected and reducing runtime errors.`,
+            `Defined custom types for more complex data structures, representing objects and other data more precisely.`,
+            `Narrowed down union types to more specific types at runtime, improving code accuracy and reducing potential bugs.`,
+            `Used interfaces and other techniques to type objects more precisely.`,
+            `Projects: Throughout the course, applied your learning in various projects like a Restaurant Recommender, 
+             TypeMart, Pixel Smile, and others. These projects helped reinforce my understanding of TypeScript in real-world scenarios.`
+          ]
+    },
     perScholas: {
         name: 'Software Engineering Training',
         level: '2024 - Per Scholas',
         image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/per-scholas-certificate.png?raw=true',
-        details: `Deep dive into web development with the MERN stack. 
-        I learned how to leverage the Node.js environment in order to use MongoDB for storing data and Express.js 
-        for delivering that data to the client while ensuring data integrity. With component-driven development in mind,
-        highly scalable and interactive React.js web applications can be enabled. 
-        With the power of these core technologies I learnt to build valuable full-stack applications.`
+        details: [
+            "Deep dive into web development with the MERN stack.",
+            `I learned how to leverage the Node.js environment to utilize MongoDB for data storage and Express.js 
+            for delivering data to the client while ensuring data integrity.`,
+            `With a focus on component-driven development, I gained the skills to build highly scalable and 
+            interactive web applications using React.js.`,
+            "With the power of these core technologies I learnt to build valuable full-stack applications."
+          ]
     },
     js2024: {
         name: 'Intermediate JavaScript',
         level: '2024 - Codecademy',
         image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/intermediateJs2024.png?raw=true',
-        details: `Learnt of the concurrency module and event loop in JavaScript. Practiced efficient error handling.
-        Used JavaScript modules to define reusable logic; identified the ways currying and hoisting contribute to 
-        how JavaScript works under the hood.`
+        details: [
+            `Learnt of the concurrency module and event loop in JavaScript.`,
+            `Practiced efficient error handling.`,
+            `Used JavaScript modules to define reusable logic.`,
+            `Identified the ways currying and hoisting contribute to how JavaScript works under the hood.`
+          ]
     },
     backendEdx2024: {
         name: 'Back-end Application Development with Node.js and Express',
         level: '2024 - IBM',
         image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/backend-edx2024.jpg?raw=true',
-        details: `Honed my skills in building robust and efficient server-side applications. 
-        Through this course, I have learned to develop applications using asynchronous callbacks and promises, 
-        ensuring smooth and responsive user experiences. Created REST APIs 
-        and performed CRUD operations, which are essential for building dynamic web applications.`
+        details: [
+            `Honed my skills in building robust and efficient server-side applications.`,
+            `Through this course, I have learned to develop applications using asynchronous callbacks and promises, ensuring smooth and responsive user experiences.`,
+            `Created REST APIs and performed CRUD operations, which are essential for building dynamic web applications.`
+          ]          
     },
     async2024: {
         name: 'JavaScript: Asynchronous Programming',
         level: '2024 - Codecademy',
         image: 'https://github.com/firstnamenika/firstnamenika.github.io/blob/main/certifications/async-2024.png?raw=true',
-        details: `Learnt how asynchronous operations work in JavaScript using callbacks, promises, and async/await syntax.
-        Used async functions and the await keyword to write asynchronous code in a more synchronous style, enhancing readability and maintainability.
-        Practiced handling errors in asynchronous JavaScript code.
-        Built a movie finder application using Fetch API upon completion.`
+        details: [
+            `Learnt how asynchronous operations work in JavaScript using callbacks, promises, and async/await syntax.`,
+            `Used async functions and the await keyword to write asynchronous code in a more synchronous style, 
+             enhancing readability and maintainability.`,
+            `Practiced handling errors in asynchronous JavaScript code.`,
+            `Built a movie finder application using Fetch API upon completion.`
+          ]
     },
     reactibm2023: {
         name: 'Developing Front End Apps with React',
         level: '2024 - IBM',
         image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/front.png',
-        details: ` Mastered the fundamentals of React.js, learning how to break 
-        down code into reusable components and effectively manage 
-        the state of elements within applications.
-         Additionally, explored Redux.js, enhancing the ability 
-         to manage complex application states.
-         Applied knowledge by developing practical projects, 
-         including the To Do List and the Shopping Cart applications. `
+        details: [
+            `Mastered the fundamentals of React.js, learning how to break down code into reusable 
+            components and effectively manage the state of elements within applications.`,
+            `Additionally, explored Redux.js, enhancing the ability to manage complex application states.`,
+            `Applied knowledge by developing practical projects, including the To Do List and the Shopping Cart applications.`
+          ]
+          
     },
     dataibm23: {
         name: 'Python Basics for Data Science',
         level: '2023 - IBM',
         image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/data-ibm-2023.png',
-        details: ` Gained a solid foundation in Python programming tailored 
-        specifically for data analysis and manipulation tasks.
-        Discovered essential libraries such as NumPy and pandas, 
-        indispensable tools for data manipulation and analysis in Python.`
+        details: [
+            `Gained a solid foundation in Python programming tailored specifically for data analysis and manipulation tasks.`,
+            `Discovered essential libraries such as NumPy and pandas, indispensable tools for data manipulation and analysis in Python.`
+          ]
     },
     python3: {
         name: 'Python 3 Course',
         level: '2023 - Codecademy',
         image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/python-2023.png',
-        details: `Learnt the fundamental concepts of Python, including variables,
-        data types, control flow structures, and functions. 
-        Built Vigenere Cipher implementation upon completion. 
-        This project not only reinforced my understanding of Python 
-        fundamentals but also provided me with knowledge that helped me acquire programmatical style of thinking.`
-    },
-    cloudComputing: {
-        name: 'Introduction to Cloud Computing',
-        level: '2023 - IBM',
-        image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/cloud-ibm-2023.png',
-        details: `Explored the fundamental principles and technologies that 
-        underpin cloud computing. From the basics of HTTP protocols 
-        to advanced security measures and encryption techniques. 
-        Gained understanding of how data is transmitted, stored, 
-        and secured in the cloud.` 
+        details: [
+            `Learnt the fundamental concepts of Python, including variables,`,
+            `data types, control flow structures, and functions.`,
+            `Built Vigenere Cipher implementation upon completion. This project not only reinforced my understanding of Python 
+            fundamentals but also provided me with knowledge that helped me acquire programmatical style of thinking.`
+          ]
     },
  };
 
@@ -83,66 +98,82 @@ export const allCertifications = {
     name: 'Intro to Next.js',
     level: '2024 - Codecademy',
     image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/learn-next2024.jpg?raw=true',
-    details: `Learnt of key concepts of Next.js, such as Server-Side Rendering, Client-Side Rendering, file-based routing system, dynamic routing, error handling etc.`
+    details: [
+        `Learnt of key concepts of Next.js, such as Server-Side Rendering, Client-Side Rendering, 
+        file-based routing system, dynamic routing, error handling etc.`
+    ]
 }, 
     express2024: {
     name: 'Learn Express',
     level: '2024 - Codecademy',
     image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/express2024.png?raw=true',
-    details: `Built an Application Programming Interface (API) in JavaScript using the popular Express framework.
-    Learnt how to use Express middleware to improve  API's code quality and readability.`
+    details: [
+        `Built an Application Programming Interface (API) in JavaScript using the popular Express framework.`,
+        `Learnt how to use Express middleware to improve  API's code quality and readability.`
+    ]
 }, 
     buildingInteractive2024: {
     name: 'Building Interactive JavaScript Websites Course',
     level: '2024 - Codecademy',
     image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/building-interactive-2024.png',
-    details: `Learnt to create webpage interactivity by leveraging JavaScript 
-    events in the browser.`
+    details: [`Learnt to create webpage interactivity by leveraging JavaScript 
+    events in the browser.`]
 },
     css2024: {
     name: 'CSS: Transitions and Animations',
     level: '2024 - Codecademy',
     image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/css2024.png?raw=true',
-    details: `Practiced implementing CSS animation using transitions.`
+    details: [`Practiced implementing CSS animation using transitions.`]
 }, 
     jsRequests2024: {
     name: 'JavaScript: Requests',
     level: '2024 - Codecademy',
     image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/jsRequests2024.png?raw=true',
-    details: `Practiced requesting data asynchronously using the async/await syntax to dynamically use data from APIs. Completed a small Film Finder project.`
+    details: [`Practiced requesting data asynchronously using the async/await syntax to dynamically use data from APIs. Completed a small Film Finder project.`]
 },
     js2024: {
     name: 'JavaScript Course',
     level: '2024 - Codecademy',
     image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/js-2024.png',
-    details: `Grasped fundamentals of JavaScript, learnt how to work with 
-    basic array methods and classes.`
+    details: [`Grasped fundamentals of JavaScript, learnt how to work with 
+    basic array methods and classes.`]
 }, 
     learnHtml2024: {
     name: 'HTML: Fundamentals',
     level: '2024 - Codecademy',
     image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/learnHtml2024.png?raw=true',
-    details: `Practiced working with HTML elements and structure, the building blocks of websites. 
-    I took this course to ensure semantic nature of my HTML structures.`
+    details: [`Practiced working with HTML elements and structure, the building blocks of websites. 
+    I took this course to ensure semantic nature of my HTML structures.`]
 }, 
     agile2023: {
     name: 'Introduction to Cloud Native, DevOps and Agile',
     level: '2023 - IBM',
     image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/agile.png',
-    details: `This course provides a comprehensive foundation in DevOps basics
-    and the principles of Agile development. 
-    Gained valuable insights into DevOps methodologies, 
-    including version control, continuous integration, 
-    and deployment strategies.
-    Learnt Agile framework, including how to effectively manage 
-    projects by writing user stories and implementing Agile practices
-    such as sprint planning, daily stand-ups, and retrospectives.`
+    details: [
+        `This course provides a comprehensive foundation in DevOps basics and the principles of Agile development.`,
+        `Gained valuable insights into DevOps methodologies, including version control, continuous integration, 
+        and deployment strategies.`,
+        `Learnt Agile framework, including how to effectively manage projects by writing user stories and 
+        implementing Agile practices such as sprint planning, daily stand-ups, and retrospectives.`
+      ]
 }, 
     webDevelopment2023: {
     name: 'Web Development with HTML5, CSS3, and JavaScript',
     level: '2023 - IBM',
     image: 'https://github.com/kweeuhree/react-website/blob/master/src/assets/certifications/introWeb2023.png?raw=true',
-    details: `Practiced and developed hands-on skills to work with HTML, CSS and JavaScript.
-    Learnt to manage projects with Git and GitHub.`
-}
+    details: [
+        `Practiced and developed hands-on skills to work with HTML, CSS, and JavaScript.`,
+        `Learnt to manage projects with Git and GitHub.`
+      ]
+},
+    cloudComputing: {
+    name: 'Introduction to Cloud Computing',
+    level: '2023 - IBM',
+    image: 'https://raw.githubusercontent.com/firstnamenika/firstnamenika.github.io/main/certifications/cloud-ibm-2023.png',
+    details: [
+        `Explored the fundamental principles and technologies that underpin cloud computing.`,
+        `From the basics of HTTP protocols to advanced security measures and encryption techniques.`,
+        `Gained understanding of how data is transmitted, stored, and secured in the cloud.`
+      ]
+},
 };
