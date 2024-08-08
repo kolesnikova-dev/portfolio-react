245c3407bb4c87db2bc60555b4db43f2a96ff8c1
Nika Kolesnikova <firstnamenika@gmail.com>
2024-08-08 12:25:52 -0400

    add catch-all route in App Routes; add _redirects for netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 // import components
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-import Skills from './components/Skills/Skills';
+// import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 // import pages
 import CertificationsPage from './pages/CertificationsPage/CertificationsPage';
@@ -30,6 +30,8 @@ function App() {
             <Route path={'/projects'} element={<Projects />} />
             <Route path={'/certifications'} element={<CertificationsPage />} />
             <Route path={'/contact'} element={<ContactPage />} />
+            {/* catch all route */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </main>
 
