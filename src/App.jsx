6c1eefcea89734049ab4fc9c6c80c287626bc9e3
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-20 11:43:22 -0400

    feat: wrap App in ErrorBoundary and comment out skills and projects routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 // import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
+// import Projects from './components/Projects/Projects';
 // import pages
-import CertificationsPage from './pages/CertificationsPage/CertificationsPage';
+// import CertificationsPage from './pages/CertificationsPage/CertificationsPage';
 import ContactPage from './pages/ContactPage/ContactPage';
 import MainPage from './pages/MainPage';
+import { ErrorBoundary } from './components/ErrorBoundary'
 //import styles
 import './App.css';
 
@@ -15,6 +16,7 @@ import './App.css';
 function App() {
 
   return (
+    <ErrorBoundary>
     <div className='App'>
       {/* navigation bar */}
       <nav>
@@ -27,8 +29,8 @@ function App() {
             <Route path={'/'} element={<MainPage />} />
             <Route path={'/home'} element={<MainPage />} />
             {/* <Route path={'/skills'} element={<Skills />} /> */}
-            <Route path={'/projects'} element={<Projects />} />
-            <Route path={'/certifications'} element={<CertificationsPage />} />
+            {/* <Route path={'/projects'} element={<Projects />} /> */}
+            {/* <Route path={'/certifications'} element={<CertificationsPage />} /> */}
             <Route path={'/contact'} element={<ContactPage />} />
             {/* catch all route */}
           <Route path="*" element={<Navigate to="/" />} />
@@ -42,6 +44,7 @@ function App() {
 
 
     </div>
+    </ErrorBoundary>
   );
 };
 
