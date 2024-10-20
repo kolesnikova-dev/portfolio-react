e2cb932f572589bab78872ea572d6f653bf6b23a
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-20 13:27:15 -0400

    update components/index exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,15 @@
 export * from './ErrorBoundary/ErrorBoundary';
 export * from './NavBar/NavBar';
 export * from './Footer/Footer';
-
+export * from './Stats/Stats';
+export * from './Skills/Skills';
+export * from './Picture/Picture';
+export * from './Loading/Loading';
+export * from './FormattedArray/FormattedArray';
+export * from './Form/Form';
+export * from './Button/Button';
+export * from './Card/Card';
+export * from './Certifications/Certifications';
+export * from './Contacts/Contact';
+export * from './Contacts/Contacts';
+// export * from './/';
