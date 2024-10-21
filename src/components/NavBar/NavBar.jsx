7e345f490dafa54b4792cc1a97890ecf1dfaef93
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-21 09:31:00 -0400

    feat: update NavBar to be a scalable solution

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,25 @@
 import { Box, Link, List, ListItem } from '@mui/material'
 import './NavBarStyle.css';
 
+const navBarLinks = {
+  Home: '/',
+  Contact: 'contact',
+}
+
+const navBarJSX = Object.entries(navBarLinks).map(([text, path]) => (
+      <ListItem key={text} className="display-flex flex-center">
+          <Link href={path}>
+            {text}
+          </Link>
+        </ListItem>
+));
+
 export const NavBar = () => {
 
   return (
     <Box component="nav" className="nav-container">
       <List className="display-flex">
-      <ListItem className="display-flex flex-center">
-          <Link href=''>
-              Home
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href='contact'>
-              Contact
-          </Link>
-        </ListItem>
+        {navBarJSX}
       </List>
     </Box>
   );
