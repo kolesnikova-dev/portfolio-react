6d05eef92eb35d787ea18dadb593338dcc85965b
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-25 13:42:03 -0400

    feat: refactor main by ensuring that an error will be thrown in case root element cannot be found

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.js'
+import App from './App.tsx';
 
 import './index.css'
-
-
 
 const rootElement = document.getElementById('root');
 try {
