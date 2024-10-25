77dc9af5deb5a46db9df56605b53aa1a3d527faf
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-25 13:41:16 -0400

    feat: update tsconfig and refactor main by ensuring that an error will be thrown in case root element cannot be found

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.js'
 
@@ -7,10 +7,19 @@ import './index.css'
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+try {
+  if(rootElement) {
+    const root = createRoot(rootElement);
+    root.render(
+      <StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </StrictMode>,
+    )
+  }
+} catch (error) {
+  throw new Error(`Could not find root element. Error: ${error}`);
+}
+
