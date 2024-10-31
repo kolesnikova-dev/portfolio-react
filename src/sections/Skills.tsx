1aae1c27c262758974f4213fce423620dfad1d6c
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-31 15:57:18 -0400

    troubleshoot

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 
 import { Typography, Tooltip } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
 
 // import { useIntersectionObserver } from '../utils';
 import { skillsData } from '../data/skillsData';
@@ -12,14 +13,23 @@ export const Skills: React.FC = () => {
 
   return (
     <section>
-      <div  className='display-flex flex-center'>
-        {Object.entries(skillsData).map(([skill, svg]) => (
-            <Tooltip title={<p>{skill}</p>} placement="bottom" arrow> 
-              {svg}
-            </Tooltip> 
+      <div className='display-flex flex-center'>
+        {skillsData.map(({skill, icon: SvgPath}) => (
+          <div
+            // ref={skillRef}
+            key={skill}
+          >
+            {/* <Typography>{skill}</Typography> */}
+            <Tooltip title={<p>{skill}</p>} placement="right" arrow>
+              {/* <IconButton sx={{ color: 'white' }} aria-label={skill}>
+                <ReactIcon fontSize="large" />
+              </IconButton> */}
+              {SvgPath}
+            </Tooltip>
+          </div>
         ))}
       </div>
-      <Typography variant="h5">Skills</Typography>
+      <Typography variant="h3">Skills</Typography>
     </section>
   );
 };
