6e936c7be1532fb138d3d160e753c4dc092cc2fc
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-31 11:06:23 -0400

    fix: remove redundant MUI components in SendEmail component

--- src/sections/SendEmail.tsx
+++ src/sections/SendEmail.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Typography, Paper} from '@mui/material';
+import { Typography, Paper} from '@mui/material';
 
 import { Form } from '../components';
 
@@ -9,24 +9,24 @@ import { lightPaperStyle } from '../MUIStyles/MUIStyles';
 
 export const SendEmail: React.FC = () => {
   return (
-    <Box component="section" className='display-flex flex-center center-column'>
+    <section className='display-flex flex-center center-column'>
     <Paper elevation={2} sx={{...lightPaperStyle, p: 4}}  className="display-flex flex-center center-column">
 
       {/* contact form container */}
-      <Box >
+      <div>
 
           {/* lets get in touch */}
-         <Box>
+         <div>
             <Typography variant="h5">
              Send me an email: 
             </Typography>
-          </Box>
+          </div>
 
           {/* form */}
           <Form />
-      </Box>
+      </div>
       </Paper>
-    </Box>
+    </section>
   )
 }
                                          
