cba20b2fa7bfdb941f313c0fa5887c2ea1007846
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-04 10:29:49 -0500

    fix: organize imports in About test

--- tests/About.test.tsx
+++ tests/About.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 
 import { About } from '../src/sections/index';
 
