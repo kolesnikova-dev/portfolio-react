f799dff001aebd74e18e98ec0090b46f2c99f6a7
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-04 10:26:40 -0500

    feat: move local styles out of index.css and into their relevant component folders

---
+++ src/components/ProjectHeader/ProjectHeader.tsx
@@ -0,0 +1,102 @@
+import Box from "@mui/material/Box";
+
+import { Project } from "../../types";
+import { icons } from "../../data/icons";
+import { IconWithTooltip } from "../index";
+
+import './projectHeaderStyles.css';
+
+
+const unfoldIconOptions = {
+    true: {
+      ariaLabel: 'See less information',
+      icon: icons.UnfoldLess,
+    },
+    false: {
+      ariaLabel: 'See more information',
+      icon: icons.UnfoldMore,
+    },
+  };
+
+  const githubIconOptions = (githubLink: string) => ({
+    icon: {
+      ariaLabel: 'View on GitHub',
+      icon: icons.GithubBlack,
+    }, 
+    link: {
+      ariaLabel: 'View on GitHub',
+      url: githubLink,
+    }
+  });
+  
+  const liveLinkOptions = (liveLink: string) => ({
+    icon: { 
+      ariaLabel: 'Navigate to live website',
+      icon: icons.ExternalLink,
+    },
+    link: {
+      ariaLabel: 'Navigate to live website',
+      url: liveLink ?? undefined,
+    }
+  });
+  
+
+type Props = {
+    project: Project,
+    fullDisplay: boolean,
+    toggleDisplay: (newIndex: number) => void,
+    index: number,
+}
+
+
+export const ProjectHeader: React.FC<Props> = ({ project, fullDisplay, toggleDisplay, index }) => {
+    const { title, githubLink, liveLink, subheader } = project;
+
+    const handleToggleDisplay = () => {
+        toggleDisplay(index);
+      };
+    
+    const titleBox = (
+        <div>
+          <span className="title">{title}</span>
+          <span> 
+            <IconWithTooltip
+              icon={unfoldIconOptions[fullDisplay.toString()]}
+              placement='right'
+            />
+          </span>
+        </div>
+      );
+
+
+      const headerFlexDirection = fullDisplay ? { xs: 'column', sm: 'column', md: 'row' } : { xs: 'row', sm: 'row' };
+
+    return (
+       <Box
+          className='display-flex flex-space bg-whitesmoke border-radius-4px pointer padding-inline-15rem padding-block-1rem'
+          flexDirection={headerFlexDirection}
+          onClick={handleToggleDisplay}
+       >
+          <div className='full-width'>
+            <div>{titleBox}</div>
+            <p className="subheader">{subheader}</p>
+          </div>
+
+          <div className={`display-flex ${!fullDisplay && 'center-column'} ${liveLink && 'flex-gap-1vw'}`}>
+              <IconWithTooltip 
+                {...githubIconOptions(githubLink)} 
+                placement='right' 
+                fullDisplay={fullDisplay}
+              />
+             {
+              liveLink && 
+                <IconWithTooltip
+                  {...liveLinkOptions(liveLink)} 
+                  placement='right'
+                  fullDisplay={fullDisplay}
+                />
+              }
+            </div>
+       </Box>
+    )
+}
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import Grid2 from '@mui/material/Grid2';
+
+import { About, Skills, Projects, Contacts, SendEmail } from '../../sections';
+import { SideBySideSection } from '../../components';
+import type { Sections } from '../../types/sectionTypes';
+
+import './mainPageStyles.css';
+
+
+const sections: Sections = {
+  top: {
+    left: { component: About, size: 6, key: 'about-section' },
+    right: { component: Skills, size: 4, key: 'skills-section' },
+  },
+  middle: {
+    component: Projects,
+    key: 'projects-section',
+  },
+  bottom: {
+    left: { component: SendEmail, size: 8, key: 'certifications-section' },
+    right: { component: Contacts, size: 4, key: 'contacts-section' },
+  },
+};
+
+
+export const MainPage: React.FC = () => {
+
+  const ProjectsSection = sections.middle.component;
+
+  return (
+    <div className="full-width flex-grow-1">
+        {/* About and Skills section */}
+        <Grid2 className="styled-grid">
+          <SideBySideSection section={sections.top} />
+        </Grid2>
+        {/* Projects section */}
+        <Grid2 className="styled-grid">
+          <ProjectsSection />
+        </Grid2>
+        {/* Contacts section */}
+        <Grid2 className="styled-grid">
+          <SideBySideSection section={sections.bottom} />
+        </Grid2>
+    </div>
+  );
+};
+
