d1cc0dfed58b054418bc5e19b5106c2f8cf94c99
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-04 13:06:18 -0400

    fix(tests): Move intersection-observer from main into test setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,22 +2,20 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/vitest';
-import { vi } from 'vitest';
+import "intersection-observer";
+import "@testing-library/jest-dom/vitest";
+import { vi } from "vitest";
 
 beforeAll(() => {
-    global.IntersectionObserver = vi.fn(() => ({
-      root: null,
-      rootMargin: '0px',
-      thresholds: [0],
-      observe: vi.fn(),
-      disconnect: vi.fn(),
-      unobserve: vi.fn(),
-      takeRecords: vi.fn(() => []),
-    }));
-    
-  });
-  
-  
+  global.IntersectionObserver = vi.fn(() => ({
+    root: null,
+    rootMargin: "0px",
+    thresholds: [0],
+    observe: vi.fn(),
+    disconnect: vi.fn(),
+    unobserve: vi.fn(),
+    takeRecords: vi.fn(() => []),
+  }));
+});
 
 window.alert = vi.fn();
