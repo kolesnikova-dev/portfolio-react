1b2e8561ab5b98fd77a546c322cf7ab9385d4cc1
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-04 13:24:52 -0400

    chore(SideBySideSection): Remove commented out code

--- src/components/SideBySideSection.tsx
+++ src/components/SideBySideSection.tsx
@@ -1,46 +1,37 @@
-import Grid2 from '@mui/material/Grid2';
+import Grid2 from "@mui/material/Grid2";
 
-import type { SideBySideSection as SideBySide} from '../types/sectionTypes';
-
+import type { SideBySideSection as SideBySide } from "../types/sectionTypes";
 
 type Props = {
-  section: SideBySide,
-}
+  section: SideBySide;
+};
 
 const fullBorderGridStyle = (section) => ({
-  width:  {
-    xs: '100%',
-    sm: '100%',
-    md: '40%',
+  width: {
+    xs: "100%",
+    sm: "100%",
+    md: "40%",
   },
-  // '--Grid-borderWidth': '1px',
-  // border: 'var(--Grid-borderWidth) solid',
-  // borderColor: 'whitesmoke',
   p: 2,
 });
-
 
 export const SideBySideSection: React.FC<Props> = ({ section }) => {
   return (
     <section>
       <Grid2
         container
-        direction={{ xs: 'column', md: 'row' }}
+        direction={{ xs: "column", md: "row" }}
         columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        rowSpacing={{ xs: 3}}
+        rowSpacing={{ xs: 3 }}
         className="display-flex flex-center"
       >
         {Object.values(section).map((side) => {
           const Component = side.component;
           return (
-            <Grid2
-              key={side.key}
-              size={side.size}
-              sx={fullBorderGridStyle}
-            >
-              <Component  /> 
-             </Grid2>
-          )
+            <Grid2 key={side.key} size={side.size} sx={fullBorderGridStyle}>
+              <Component />
+            </Grid2>
+          );
         })}
       </Grid2>
     </section>
