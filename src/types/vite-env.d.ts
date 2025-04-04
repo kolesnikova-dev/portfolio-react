172334823abbfee4d1d1d1200d531db5bfdb9bd3
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-04 14:32:18 -0400

    style(vite.d.ts): Prettier auto-format

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,22 +1,22 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    VITE_PUBLIC_KEY: string;
-    VITE_SERVICE_ID: string;
-    VITE_TEMPLATE_ID: string;
-    VITE_SMART_SHOP_ID: string;
-    VITE_VIGENERE_ID: string;
-    VITE_WEATHER_ID: string;
-    VITE_CLIMATE_ID: string;
-    VITE_CLIMATE_THUMBNAILS_ID: string;
-    VITE_SMART_SHOP_THUMBNAILS_ID: string;
-    VITE_VIGENERE_THUMBNAILS_ID: string;
-    VITE_WEATHER_THUMBNAILS_ID: string;
-    VITE_CLOUDINARY_BASE_URL: string;
-    VITE_AVATAR_ID: string;
-  }
-  
+  VITE_PUBLIC_KEY: string;
+  VITE_SERVICE_ID: string;
+  VITE_TEMPLATE_ID: string;
+  VITE_SMART_SHOP_ID: string;
+  s;
+  VITE_VIGENERE_ID: string;
+  VITE_WEATHER_ID: string;
+  VITE_CLIMATE_ID: string;
+  VITE_CLIMATE_THUMBNAILS_ID: string;
+  VITE_SMART_SHOP_THUMBNAILS_ID: string;
+  VITE_VIGENERE_THUMBNAILS_ID: string;
+  VITE_WEATHER_THUMBNAILS_ID: string;
+  VITE_CLOUDINARY_BASE_URL: string;
+  VITE_AVATAR_ID: string;
+}
+
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
+  readonly env: ImportMetaEnv;
+}
