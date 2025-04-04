2ac4153c3136be5b5a7c408e7644d7c3ae03c653
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-04 16:40:54 -0400

    feat: Uninstall three/postprocessing and imports Stars component directly

--- src/components/ParticlesCanvas.tsx
+++ src/components/ParticlesCanvas.tsx
@@ -1,8 +1,7 @@
 import { useRef } from "react";
 import { Group } from "three";
-import { Stars } from "@react-three/drei";
+import { Stars } from "@react-three/drei/core/Stars";
 import { Canvas, useFrame } from "@react-three/fiber";
-// import { EffectComposer, Bloom } from '@react-three/postprocessing';
 
 const Particles = () => {
   const groupRef = useRef<Group>(null!);
@@ -29,31 +28,6 @@ const Particles = () => {
   );
 };
 
-// const color = new THREE.Color(2, 10, 10);
-
-// const ShootingStar = () => {
-//   const ref = useRef<THREE.Mesh>(null);
-
-//   useFrame((state) => {
-//     const t = state.clock.getElapsedTime() * 0.5; // speed factor set to 1 is default speed
-//     if (ref.current) {
-//       ref.current.position.set(
-//         Math.sin(t) * 20,  // x-axis: horizontal range
-//         Math.atan(t) * Math.cos(t / 2) * 20, // y-axis: vertical range
-//         Math.cos(t) * 200  // z-axis: depth movement
-//       );
-//     }
-//   });
-//   return (
-//     <Trail width={5} length={2} color={color} attenuation={(t) => t * t}>
-//       <mesh ref={ref}>
-//         <sphereGeometry args={[0.25]} />
-//         <meshBasicMaterial color={[3, 10, 5]} toneMapped={false} />
-//       </mesh>
-//     </Trail>
-//   );
-// }
-
 const ParticlesCanvas = () => {
   return (
     <Canvas
@@ -66,14 +40,6 @@ const ParticlesCanvas = () => {
         zIndex: -1,
       }}
     >
-      {/* <ambientLight intensity={0.5} />
-            
-                <ShootingStar />
-            
-                <EffectComposer>
-                    <Bloom mipmapBlur luminanceThreshold={0.2} intensity={0.4} />
-                </EffectComposer> */}
-
       <Particles />
     </Canvas>
   );
