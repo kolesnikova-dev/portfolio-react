f6766f558119157b87b6d6955e9211544dc32ae9
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-14 15:33:53 -0400

    update(utils): Add Distributed Rate Limiter cloudinary data

--- src/utils/cloudinaryUtils.ts
+++ src/utils/cloudinaryUtils.ts
@@ -1,12 +1,12 @@
 //import cloudinary ids
 const cloudinaryMediaIds = {
   smartShopRecording: import.meta.env.VITE_SMART_SHOP_ID,
-  vigenereRecording: import.meta.env.VITE_VIGENERE_ID,
+  limiterRecording: import.meta.env.VITE_LIMITER_ID,
   weatherRecording: import.meta.env.VITE_WEATHER_ID,
   budgetingRecording: import.meta.env.VITE_BUDGETING_ID,
   budgetingThumbnails: import.meta.env.VITE_BUDGETING_THUMBNAILS_ID,
   smartShopThumbnails: import.meta.env.VITE_SMART_SHOP_THUMBNAILS_ID,
-  vigenereThumbnails: import.meta.env.VITE_VIGENERE_THUMBNAILS_ID,
+  limiterThumbnails: import.meta.env.VITE_LIMITER_THUMBNAILS_ID,
   weatherThumbnails: import.meta.env.VITE_WEATHER_THUMBNAILS_ID,
   avatar: import.meta.env.VITE_AVATAR_ID,
 };
