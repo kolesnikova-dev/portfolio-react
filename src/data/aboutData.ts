2a35a3964bbabc68d53acb44a6587f1ddcf2cbea
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-24 11:44:52 -0400

    chore(data): Update intro data

--- src/data/aboutData.ts
+++ src/data/aboutData.ts
@@ -1,6 +1,6 @@
 export const aboutData = {
   greeting: "Hi, I'm Nika!",
-  introduction: `I love building things for the web and figuring out how it all works under the hood. 
-  Hard work and curiousity keep me going. 
+  introduction: `I'm a self-taught developer who loves solving problems and turning ideas into real, working things. 
+  I'm curious by nature, motivated by progress, and always up for a challenge. 
   Welcome!`,
 };
