133470d85492913fc9259257adc48d912eff0a29
Nika Kolesnikova <firstnamenika@gmail.com>
2025-05-06 11:47:01 -0400

    chore(types): Add ProjectDetails interface

--- src/types/projectTypes.ts
+++ src/types/projectTypes.ts
@@ -1,11 +1,13 @@
+export interface ProjectDetails {
+  [key: string]: string | string[];
+}
+
 export interface Project {
   title: string;
   subheader: string;
   githubLink: string;
   thumbnails: string[];
-  details: {
-    [key: string]: string;
-  };
+  details: ProjectDetails;
   liveLink?: string;
   video?: string[];
 }
