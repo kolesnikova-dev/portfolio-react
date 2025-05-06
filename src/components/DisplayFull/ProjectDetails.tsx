b52c98c557a01829c5b74e6a4e2945475f43837b
Nika Kolesnikova <firstnamenika@gmail.com>
2025-05-06 12:12:39 -0400

    style(ProjectDetails): Update the way Core Features is displayed

--- src/components/DisplayFull/ProjectDetails.tsx
+++ src/components/DisplayFull/ProjectDetails.tsx
@@ -1,12 +1,7 @@
 import { TableCell, TableContainer, TableRow } from "@mui/material";
+import type { ProjectDetails as Details } from "../../types";
 
-type Props = {
-  [key: string]: {
-    [key: string]: string;
-  };
-};
-
-export const ProjectDetails: React.FC<Props> = ({ details }) => {
+export const ProjectDetails = ({ details }: { details: Details }) => {
   return (
     <div className="flex-1 pd-05rem margin-top-2rem">
       <div className="test-background flex-column center-column">
@@ -17,7 +12,20 @@ export const ProjectDetails: React.FC<Props> = ({ details }) => {
                 <TableRow key={point}>
                   <TableCell className="border">
                     <h2>{point}</h2>
-                    <p className="md-font">{detail}</p>
+                    {Array.isArray(detail) ? (
+                      <>
+                        {detail.map((sentence) => (
+                          <p
+                            key={sentence.slice(10)}
+                            className="margin-block-1rem md-font"
+                          >
+                            • {sentence}
+                          </p>
+                        ))}
+                      </>
+                    ) : (
+                      <p className="md-font">{detail}</p>
+                    )}
                   </TableCell>
                 </TableRow>
               ))}
