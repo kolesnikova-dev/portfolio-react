92eb6369e90a9e074000aaa6da93cee014ac5c3d
Nika Kolesnikova <firstnamenika@gmail.com>
2025-05-07 16:00:51 -0400

    refactor(Skills): Add animation to each skill

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,21 +1,60 @@
-import Tooltip from "@mui/material/Tooltip";
+import { useEffect, useState } from "react";
+
+import Box from "@mui/material/Box";
 
 import { skillsData } from "../data/skillsData";
 
+const SKILLS_HEADER = "Skills";
+
 export const Skills: React.FC = () => {
+  const [isHovered, setHover] = useState<string>("");
+  const [currentIndex, setCurrentIndex] = useState<number>(-1);
+
+  const toggleHover = (skill: string) => setHover(skill);
+
+  useEffect(() => {
+    const delay = currentIndex === -1 ? 2200 : 1800; // Display header for longer than skills
+    const timer = setTimeout(() => {
+      setCurrentIndex((prev) => {
+        if (prev >= skillsData.length - 1) {
+          return -1;
+        }
+        return prev + 1;
+      });
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [currentIndex]);
+
   return (
     <section>
+      <div className="display-flex flex-center flex-gap-2rem">
+        {skillsData.map(({ skill, icon: SvgPath }, index) => (
+          <div
+            className={`rd-transparent-border green-box-shadow pd-inline-15rem pd-05rem ${currentIndex === index && "glow"} hover-glow pointer`}
+            key={skill}
+          >
+            <Box
+              component="p"
+              onMouseEnter={() => toggleHover(skill)}
+              onMouseLeave={() => toggleHover("")}
+              aria-label={skill}
+              className="pointer"
+            >
+              {SvgPath}
+            </Box>
+          </div>
+        ))}
+      </div>
+
       <header>
-        <div className="display-flex flex-center flex-gap-1rem">
-          {skillsData.map(({ skill, icon: SvgPath }) => (
-            <h2 aria-label={skill} key={skill} className="pointer">
-              <Tooltip title={<p>{skill}</p>} placement="bottom" arrow>
-                {SvgPath}
-              </Tooltip>
-            </h2>
-          ))}
-        </div>
-        <h2>Skills</h2>
+        <h2>
+          {isHovered
+            ? isHovered
+            : currentIndex >= 0
+              ? skillsData[currentIndex].skill
+              : SKILLS_HEADER}
+        </h2>
       </header>
     </section>
   );
