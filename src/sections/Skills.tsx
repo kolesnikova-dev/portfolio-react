4d52efd5beae1fbc3b34ca7be5a8c64a8223ba53
Nika Kolesnikova <firstnamenika@gmail.com>
2025-05-07 17:37:05 -0400

    style(Skills): Add flex-wrap to skills container

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -5,19 +5,20 @@ import Box from "@mui/material/Box";
 import { skillsData } from "../data/skillsData";
 
 const SKILLS_HEADER = "Skills";
+const HEADER_INDEX = -1;
 
 export const Skills: React.FC = () => {
   const [isHovered, setHover] = useState<string>("");
-  const [currentIndex, setCurrentIndex] = useState<number>(-1);
+  const [currentIndex, setCurrentIndex] = useState<number>(HEADER_INDEX);
 
   const toggleHover = (skill: string) => setHover(skill);
 
   useEffect(() => {
-    const delay = currentIndex === -1 ? 2200 : 1800; // Display header for longer than skills
+    const delay = currentIndex === HEADER_INDEX ? 2200 : 1800; // Display header for longer than skills
     const timer = setTimeout(() => {
       setCurrentIndex((prev) => {
         if (prev >= skillsData.length - 1) {
-          return -1;
+          return HEADER_INDEX;
         }
         return prev + 1;
       });
@@ -28,12 +29,18 @@ export const Skills: React.FC = () => {
 
   return (
     <section>
-      <div className="display-flex flex-center flex-gap-2rem">
+      {/* All skills container */}
+      <Box
+        className="display-flex flex-center flex-gap-2rem"
+        flexWrap={{ xs: "wrap", lg: "nowrap" }}
+      >
         {skillsData.map(({ skill, icon: SvgPath }, index) => (
+          // A specific skill container -- green shadow box
           <div
             className={`rd-transparent-border green-box-shadow pd-inline-15rem pd-05rem ${currentIndex === index && "glow"} hover-glow pointer`}
             key={skill}
           >
+            {/* A specific skill */}
             <Box
               component="p"
               onMouseEnter={() => toggleHover(skill)}
@@ -41,11 +48,12 @@ export const Skills: React.FC = () => {
               aria-label={skill}
               className="pointer"
             >
+              {/* Skill svg */}
               {SvgPath}
             </Box>
           </div>
         ))}
-      </div>
+      </Box>
 
       <header>
         <h2>
