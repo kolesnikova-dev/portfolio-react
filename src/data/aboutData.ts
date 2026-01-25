c204e5136492f23b89be24bd409973a89ea3ccda
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2026-01-25 11:36:16 -0500

    chore(data): Update aboutData and skillsData

--- src/data/aboutData.ts
+++ src/data/aboutData.ts
@@ -1,6 +1,6 @@
 export const aboutData = {
   greeting: "Hi, I'm Nika!",
-  introduction: `I'm a self-taught developer who loves solving problems and turning ideas into real, working things. 
+  introduction: `I'm an Associate Software Engineer. 
   I'm curious by nature, motivated by progress, and always up for a challenge. 
   Welcome!`,
 };
